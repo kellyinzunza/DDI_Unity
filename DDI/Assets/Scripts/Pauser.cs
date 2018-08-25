1f98dc20bf598a6a446d99088afc7a43e8fd05bb
Kelly Inzunza <kelly.inzunza@uabc.edu.mx>
2018-08-24 19:43:00 -0700

    Practica 1
    Se modifico el juego. Se agregaron botones para hacer pausa, reiniciar juego y el sonido

--- DDI/Assets/Scripts/Pauser.cs
+++ DDI/Assets/Scripts/Pauser.cs
@@ -1,19 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Pauser : MonoBehaviour {
 	private bool paused = false;
+	public GameObject pausePanel;
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.P))
 		{
-			paused = !paused;
+			paused = !paused; //con la tecla t
+			pausePanel.SetActive(paused);
 		}
 
 		if(paused)
-			Time.timeScale = 0;
+			Time.timeScale = 0; //lentp
 		else
-			Time.timeScale = 1;
+			Time.timeScale = 1;	//rapido
 	}
+	
+	public void ContinueGame(){
+			Debug.Log("Continuar...");
+			pausePanel.SetActive(paused);
+		
+	}
+	
+	public void ResetGame(){
+			Debug.Log("ResetGame...");
+			SceneManager.LoadScene(0);
+		
+	}
+	
+	public void MuteMusic(){
+			Debug.Log("Mute music");
+		
+	}
+	
+	
 }
